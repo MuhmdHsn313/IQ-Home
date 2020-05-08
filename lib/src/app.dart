003163be4e0589ab9a167ed620c7f226c8a57096
Muhammad Hasan Alasady <muhmdhsn313@gmail.com>
2020-05-08 05:00:37 +0300

    Setup file structure & dependencies.

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IQHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
