8c888d35e52edf902a2d0583bb6e41996d7a66b4
Muhammad Hasan Alasady <muhmdhsn313@gmail.com>
2020-05-23 04:50:36 +0300

    Register adapters and current application folder in `Hive`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:iqhome/src/models/section.dart';
+import 'package:iqhome/src/models/source.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'src/app.dart';
+import 'src/models/tip.dart';
+import 'src/models/qanda.dart';
+import 'src/models/news.dart';
 
-void main() {
+void main() async {
+  final currentDir = await getApplicationDocumentsDirectory();
+  Hive.init(currentDir.path);
+  Hive.registerAdapter(NewsAdapter());
+  Hive.registerAdapter(TipAdapter());
+  Hive.registerAdapter(QandAAdapter());
+  Hive.registerAdapter(SectionAdapter());
+  Hive.registerAdapter(SourceAdapter());
   runApp(IQHome());
 }
